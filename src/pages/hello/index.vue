0a3a34d221c2d97e13783d73855375772aba909f
Nikita Nedelko <93500964+weekkkk@users.noreply.github.com>
2025-04-08 00:20:13 +0300

    DRtji433 / Инициализация - Стили текста - :root размер шрифта (резина)

--- src/pages/hello/index.vue
+++ src/pages/hello/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2>Hello Page!</h2>
+  <h2 class="text-6xl">
+    Hello Page!
+  </h2>
   <HelloWorldWidget />
   <HelloWorldFeature />
   <HelloWorld />
