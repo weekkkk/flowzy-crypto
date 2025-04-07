a11fe6754b7f755c4cff1ad637498508f311b246
Nikita Nedelko <93500964+weekkkk@users.noreply.github.com>
2025-04-08 01:42:11 +0300

    DRtji433 / Инициализация - Добавил пример для тестовых данных запросов

---
+++ server/api/hello/get-message.ts
@@ -0,0 +1,5 @@
+export default defineEventHandler(() => {
+  return {
+    value: "Hello World!",
+  };
+});
--- src/widgets/hello/ui/world/index.vue
+++ src/widgets/hello/ui/world/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const { data: message } = useAsyncData ("hello-get-message", () => HelloService.getMessage());
+</script>
+
 <template>
   <h3>Hello World Widget</h3>
+  <details class="bg-primary-400">
+    <summary>
+      Mock <strong>HelloService.getMessage</strong> Response
+    </summary>
+    <p class="text-xl">
+      {{ message?.value }}
+    </p>
+  </details>
 </template>
