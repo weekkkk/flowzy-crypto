ef583b5dfb666512ef288fc179a1cfeb82e0098c
AlexeyFrunza <tvoih8r@mail.ru>
2025-04-09 23:59:23 +0300

    [KRW35JP3] Дашборд для гостя -- Добавлена страница гостя -- Добавлен виджет AuthStart -- Добавлен виджет DashboardInfo, DashboardVideo -- Начата верстка виджета StatGeneral

---
+++ src/pages/dashboard/disconnected/index.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+  <div class="w-full flex flex-col items-center mt-37.5">
+    <div class="w-full">
+      <AuthStartWidget class="mb-37.5" />
+      <div class="flex justify-between items-end">
+        <div>
+          <DashboardUiInfoPage />
+          <DashboardUiVideoPage class="mt-13.5" />
+        </div>
+        <div>
+          <StatGeneralWidget />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/widgets/stat/ui/general/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="w-115 h-121.75 bg-neutral-700 rounded-5xl px-12.5 pt-12.5">
+    <div>
+      <h5 class="text-neutral-400 opacity-60">
+        Total participants
+      </h5>
+    </div>
+    <div>
+      <h5 class="text-neutral-400 opacity-60">
+        Total transactions
+      </h5>
+    </div>
+    <div>
+      <h5 class="text-neutral-400 opacity-60">
+        <span class="bg-gradient-to-r primary-gradient bg-clip-text text-transparent !opacity-100">SOL</span> turnover
+      </h5>
+    </div>
+  </div>
+</template>
