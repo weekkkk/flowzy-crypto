e8eecba736ae31a8882be218fc9de2a8c601153e
AlexeyFrunza <tvoih8r@mail.ru>
2025-04-09 23:59:23 +0300

    [KRW35JP3] Дашборд для гостя -- Добавлена страница гостя -- Добавлен виджет AuthStart -- Добавлен виджет DashboardInfo, DashboardVideo -- Начата верстка виджета StatGeneral

---
+++ src/entities/stat/ui/card/index.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="w-115 h-121.75 bg-neutral-700 rounded-5xl px-12.5 pt-12.5 flex flex-col">
+    <div class="pb-5">
+      <h5 class="text-2xl text-neutral-400 opacity-60">
+        Total participants
+      </h5>
+      <p class="text-4xl">
+        8,932
+      </p>
+      <p class="text-lg bg-gradient-to-r primary-gradient bg-clip-text text-transparent">
+        + 3,217
+      </p>
+    </div>
+    <USeparator />
+    <div class="pb-5 pt-6.25">
+      <h5 class="text-2xl text-neutral-400 opacity-60">
+        Total transactions
+      </h5>
+      <p class="text-4xl">
+        13,286
+      </p>
+      <p class="text-lg bg-gradient-to-r primary-gradient bg-clip-text text-transparent">
+        + 9,833
+      </p>
+    </div>
+    <USeparator />
+    <div class="pb-5 pt-6.25">
+      <h5 class="text-2xl text-neutral-400 ">
+        <span class="bg-gradient-to-r primary-gradient bg-clip-text text-transparent">SOL </span>
+        <span class="opacity-60">turnover</span>
+      </h5>
+      <p class="text-4xl">
+        7,265
+      </p>
+      <p class="text-lg bg-gradient-to-r primary-gradient bg-clip-text text-transparent">
+        + 4,201
+      </p>
+    </div>
+  </div>
+</template>
