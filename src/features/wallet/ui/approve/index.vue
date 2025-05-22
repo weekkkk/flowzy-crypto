3b2dbf3f7179db7c897dc5dac1cab91a4379ddbe
AlexeyFrunza <tvoih8r@mail.ru>
2025-05-22 19:02:25 +0300

    [XAlHSJvB] Добавить отступ между границей поля и кнопкой -- добавил отступ для кнопки в WalletApproveFeature

--- src/features/wallet/ui/approve/index.vue
+++ src/features/wallet/ui/approve/index.vue
@@ -22,7 +22,7 @@ async function onCheck() {
     <template #trailing>
       <UButton
         label="Approve"
-        class="w-40 h-15.5 flex justify-center cursor-pointer rounded-3xl max-md:w-26.5 max-md:h-12 max-md:text-sm max-md:rounded-2xl max-md:mr-0.25"
+        class="w-40 h-15.5 flex justify-center mr-1 cursor-pointer rounded-3xl max-md:w-26.5 max-md:h-12 max-md:text-sm max-md:rounded-2xl max-md:mr-0.25"
         @click="onCheck"
       />
     </template>
