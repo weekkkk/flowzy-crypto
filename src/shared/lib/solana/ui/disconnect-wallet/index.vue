ddf09f47896da4e58bdc3a5806bb11eaacb0186c
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-09-05 00:36:55 +0300

    mbkcIN0W: При выходе из аккаунта нужно перекидывать на главную страницу

--- src/shared/lib/solana/ui/disconnect-wallet/index.vue
+++ src/shared/lib/solana/ui/disconnect-wallet/index.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+const emit = defineEmits<{
+  (e: "disconnect"): void;
+}>();
+
 const { disconnect } = useExtendedWallet();
+
+async function onCLick() {
+  emit("disconnect");
+  await disconnect();
+}
 </script>
 
 <template>
-  <UButton color="neutral" class="bg-neutral-800 p-5 max-md:p-3 h-max rounded-5xl max-md:rounded-4xl text-neutral-200 cursor-pointer" @click="disconnect">
+  <UButton
+    color="neutral"
+    class="!bg-neutral-800 p-5 max-md:p-3 h-max rounded-5xl max-md:rounded-4xl text-neutral-200 cursor-pointer"
+    @click="onCLick"
+  >
     <UIcon name="fci:logout" class="w-8 h-8 max-md:w-6 max-md:h-6" />
   </UButton>
 </template>
